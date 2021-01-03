e1a4869aae23dfd3328ecdaf897dc6b1e9cec41f
Siddharth Mishra <smishra1_be18@thapar.edu>
2021-01-03 23:18:23 +0530

    rand

---
+++ geany/t1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<cstring>
+
+//#include<conio>
+//geany compiler (mingw ??)
+
+using namespace std;
+int switch1( int a ,int b)
+		{
+	     
+	     int ab = a*b - a + b;
+	     int bc = a+b;
+	     
+	     if ( ab > bc) return 0;
+	     
+	     else return 1;
+	     		
+			
+		}
+int main()
+
+{
+   char a[10] ={"helllo"};
+   char b[10] ;
+   strcpy(a,b);
+   		
+
+  int c = switch1(5,5);
+  cout << c;
+
+
+	return 0;
+	
+	}
